4b39c18da865ba4b542d0d5a7d49378cbd58757c
cauabackend <cauabackend@gmail.com>
2026-06-09 08:41:18 -0300

    build: configure vite with the react plugin

---
+++ vite.config.js
@@ -0,0 +1,7 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// Configuração padrão do Vite com o plugin oficial do React.
+export default defineConfig({
+  plugins: [react()],
+})
