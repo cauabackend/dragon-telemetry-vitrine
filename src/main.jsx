bd76e593016cf84eb0835646bb4df87a0c9c8926
cauabackend <cauabackend@gmail.com>
2026-06-09 08:41:20 -0300

    feat: bootstrap React app with BrowserRouter

---
+++ src/main.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
+import App from './App.jsx'
+import './index.css'
+
+// Ponto de entrada da aplicação.
+// O <BrowserRouter> habilita as rotas reais (URLs navegáveis pelo React Router).
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+)
